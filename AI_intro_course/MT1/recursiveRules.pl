a43ad9d48370c2429e18372a5899bbb9dd7dad10
Jesper Isacson <isul18wj@student.ju.se>
2022-09-16 16:11:20 +0200

    recursive rules

--- AI_intro_course/MT1/recursiveRules.pl
+++ AI_intro_course/MT1/recursiveRules.pl
@@ -1,55 +1,92 @@
-/*
-X = and(ge(a,b),c)
-*/
-convert(X) :-
-    
-
-
-/*go(Expression,Result):-
-    convert_term(Expression,Result),
+convert(Expression):-
+    convert(Expression, Result),
     output_term(Result).
-convert_term(add(Val1,Val2),Result) :-
+
+convert(Expression,Result):-
+    convert_term(Expression,Result).
+
+convert_term(le(Val1,Val2),Result) :-
     convert_term(Val1,ResultVal1),
     convert_term(Val2,ResultVal2),
-    Result = ResultVal1+ResultVal2.
-convert_term(sub(Val1,Val2),Result) :-
+    Result = (ResultVal1=<ResultVal2).
+
+convert_term(eq(Val1,Val2),Result) :-
     convert_term(Val1,ResultVal1),
     convert_term(Val2,ResultVal2),
-    Result = ResultVal1-ResultVal2.
-convert_term(div(Val1,Val2),Result) :-
+    Result = (ResultVal1==ResultVal2).
+
+convert_term(ne(Val1,Val2),Result) :-
     convert_term(Val1,ResultVal1),
     convert_term(Val2,ResultVal2),
-    Result = ResultVal1/ResultVal2.
-convert_term(mult(Val1,Val2),Result) :-
+    Result = (ResultVal1=\=ResultVal2).
+
+convert_term(ge(Val1,Val2),Result) :-
     convert_term(Val1,ResultVal1),
     convert_term(Val2,ResultVal2),
-    Result = ResultVal1*ResultVal2.
+    Result = (ResultVal1>=ResultVal2).
+
+convert_term(and(Val1,Val2),Result) :-
+    convert_term(Val1,ResultVal1),
+    convert_term(Val2,ResultVal2),
+    Result = ResultVal1 + '&' + ResultVal2.
+
+convert_term(or(Val1,Val2),Result) :-
+    convert_term(Val1,ResultVal1),
+    convert_term(Val2,ResultVal2),
+    Result = ResultVal1 + '||' + ResultVal2.
+
+convert_term(not(Val1),Result) :-
+    convert_term(Val1,ResultVal1),
+    Result = '!' + ResultVal1.    
+
 convert_term(X,Result):-
     Result = X.
-output_term(Val1+Val2) :-
-    write("add("),
+
+output_term(Val1=<Val2) :-
+    write('('),
     output_term(Val1),
-    write(","),
+    write(' =< '),
     output_term(Val2),
-    write(")").
-output_term(Val1-Val2) :-
-    write("subß("),
+    write(')').
+
+output_term(Val1==Val2) :-
+    write('('),
     output_term(Val1),
-    write(","),
+    write(' == '),
     output_term(Val2),
-    write(")").
-output_term(Val1*Val2) :-
-    write("mult("),
+    write(')').
+
+output_term(Val1=\=Val2) :-
+    write('('),
     output_term(Val1),
-    write(","),
+    write(' <> '),
     output_term(Val2),
-    write(")").
-output_term(Val1/Val2) :-
-    write("div("),
+    write(')').
+
+output_term(Val1>=Val2) :-
+    write('('),
     output_term(Val1),
-    write(","),
+    write(' >= '),
     output_term(Val2),
-    write(")").
+    write(')').
+
+output_term(Val1 + '&' + Val2) :-
+    write('('),
+    output_term(Val1),
+    write(' & '),
+    output_term(Val2),
+    write(')').
+
+output_term(Val1 + '||' + Val2) :-
+    write('('),
+    output_term(Val1),
+    write(' || '),
+    output_term(Val2),
+    write(')').
+
+output_term('!'+Val1) :-
+    write('!'),
+    output_term(Val1).
+
 output_term(X):-
     write(X).
-*/
